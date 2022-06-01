3b7e0ba01868a6ca70027f840385c2d69b3f9295
Bas <alphadragon321@gmail.com>
2022-06-01 13:22:04 +0700

    add update userData api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 import { User } from './model/user-model';
+import { UserUpdateDto } from './model/update-uesr-model'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   // @Get()
   // getHello(): string {
   //   return this.appService.getHello();
   // }
-  
+
   @Get()
   getUser() {
     return this.appService.listUser();
@@ -19,5 +20,10 @@ export class AppController {
   @Post()
   createUser(@Body() userDto: User): Promise<User> {
     return this.appService.createUser(userDto)
+  }
+
+  @Put(':id')
+  updateUser(@Param("id") id: string, @Body() UserUpdateDto: UserUpdateDto): Promise<User> {
+    return this.appService.updateUser(id, UserUpdateDto)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { UserUpdateDto } from './model/update-uesr-model';
 import { User, userDocument } from './model/user-model';
 
 @Injectable()
 export class AppService {
   constructor(
-    @InjectModel("user") private readonly userModel:Model<userDocument>
-  ){}
+    @InjectModel("user") private readonly userModel: Model<userDocument>
+  ) { }
   getHello(): string {
     return 'Hello World!';
   }
 
-  async listUser(){
+  async listUser() {
     return this.userModel.find({}).then(user => user).catch(err => console.log(err))
   }
 
   async createUser(body): Promise<User> {
     const newUser = new this.userModel(body);
     return newUser.save()
+  }
+
+  async updateUser(id: string, updateBody: UserUpdateDto): Promise<User> {
+    return this.userModel.findByIdAndUpdate(id, updateBody, { new: true })
   }
 }
